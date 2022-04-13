c7a427967574d60c1df2d7c7d96c6e6b24b5cbca
Jerod <jerod.durham@jasperproducts.com>
2022-04-13 16:42:06 +0000

    save 1

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function updateRate() {
+    // Update rate when slider is moved.
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateVal + " %"
+  }
+  
+  function compute() {
+    // define variables 
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+  
+    if (principal <= 0) {
+      // Input validation
+      alert("Enter a positive number");
+      document.getElementById("principal").focus();
+    } else {
+      // Compute values
+      document.getElementById("result").innerHTML = `<br />
+      If you deposit <span class="highlight">` + principal + `</span>, <br />
+      at an interest rate of <span class="highlight">` + rate + `</span>, <br />
+      You will receive an amount of <span class="highlight">` + interest + `</span>, <br />
+      in the year <span class="highlight">` + year + `</span>`;
+    }
 }
-        
